184228eaeae9378a5ee80b5d0d15411037cbecbb
PauloArthur <pauloarthurcostamendes@gmail.com>
2018-08-05 16:24:27 -0300

    "Adapts the Survivors and Witnesses controllers to  change de reports"

--- app/controllers/api/v1/survivors_controller.rb
+++ app/controllers/api/v1/survivors_controller.rb
@@ -9,7 +9,7 @@ module Api
         survivors = Survivor.all
         render_success('Survivors Loaded', survivors)
       end
-      
+
       # Get request on '/:id'
       def show
         survivor = Survivor.find(params[:id])
@@ -20,17 +20,20 @@ module Api
       def create
 
         # Sets abducted to false, as there is no need to pass it by request
-        surv = survivor_params
+        surv = survivor_params()
         surv[:abducted] = false
         survivor = Survivor.new(surv)
 
-        # Creates an adds the Localization to the Survivor
+        # Creates an adds the Localization of the Survivor
         survivor.localization = Localization.new({
           latitude: params[:latitude],
           longitude: params[:longitude]
         })
 
         if survivor.save
+          # Adds 1 on the survivors_quantity of report
+          update_survivors_reports(+1, survivor.abducted)
+
           render_success('Survivor Saved', survivor)
         else
           render_failure('Survivor not saved', survivor)
@@ -51,12 +54,38 @@ module Api
       # Delete request on '/:id
       def destroy
         survivor = Survivor.find(params[:id])
+
+        # Removes 1 from survivors_quantity and, if the survivor was one
+        # abducted, removes from abducted_quantity
+        update_survivors_reports(-1, survivor.abducted)
+
         survivor.destroy
 
         render_success('Survivor Deleted', survivor)
       end
 
       private
+
+      def create_report(value)
+        report = Report.new({
+          abducted_quantity: 0,
+          survivors_quantity: value
+        })
+        report.save ? '' : render_failure("Report couldn't be initialized", report)
+      end
+
+      def update_survivors_reports(value, is_abducted)
+
+        # Creates a report if no one exists
+        if Report.all.size.zero?
+          create_report(value)
+        else
+          report = Report.find(1)
+          update_param = {survivors_quantity: report.survivors_quantity + value}
+          update_param[:abducted_quantity] = report.abducted_quantity + value if is_abducted
+          report.update(update_param) ? '' : render_failure("Report couldn't be updated", report)
+        end
+      end
 
       def survivor_params
         params.require(:survivor).permit(:name, :gender, :age)
--- app/controllers/api/v1/witnesses_controller.rb
+++ app/controllers/api/v1/witnesses_controller.rb
@@ -25,26 +25,19 @@ module Api
         survivor = Survivor.find(survivor_id)
 
         # If id of witness doesn't belong in the survivors
-        unless Survivor.find_by(id: witness_id)
-          render_failure('ERROR: ID of witness doesn\'t exist', survivor)
-          return
-        end
+        survivor_not_belong(survivor, witness_id)
 
         # If the survivor IS its own witness
         if survivor_id == witness_id
           render_failure('ERROR: Survivors can\'t witness for themselves', survivor)
           return
-        
         # If the witness hasn't already witnessed
         elsif !survivor.witnesses.find_by(witness_id: witness_id)
-          
-          # Saves abducted => true when survivor has 3 or more witnesses
-          survivor[:abducted] = true
-          survivor.save if survivor.witnesses.size == 2
-
           # Creates witness in survivor
           witness = survivor.witnesses.create(witness_params)
           if witness.save
+            # Saves abducted => true when survivor has 3 or more witnesses
+            abducting_survivor(survivor)
             render_success('Witness Saved', witness)
           else
             render_failure('Witness not Saved', witness)
@@ -57,6 +50,30 @@ module Api
       # There is no need for update or destroy
 
       private
+
+      # Adds 1 to abducted_quantity of repost
+      def update_abducted_reports
+        report = Report.find(1)
+        update_param = {abducted_quantity: report.abducted_quantity + 1}
+        report.update(update_param) ? '' : render_failure("Report couldn't be updated", report)
+      end
+
+      def abducting_survivor(survivor)
+        if survivor.witnesses.size == 2
+          if survivor.update({abducted: true})
+            update_abducted_reports()
+          else
+            render_failure("Survivor couldn't be updated", survivor)
+          end
+        end
+      end
+
+      def survivor_not_belong(survivor, witness_id)
+        unless Survivor.find_by(id: witness_id)
+          render_failure('ERROR: ID of witness doesn\'t exist', survivor)
+          return
+        end
+      end
 
       def witness_params
         params.require(:witness).permit(:witness_id)
